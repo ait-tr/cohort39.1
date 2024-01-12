77d2166bab223ac30c537aad0d7b256bfacbf1e4
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-12 17:11:01 +0100

    lesson 24

--- basic_programming/homework_solutions/test/lesson21/OOP/electronics/TabletTest.java
+++ basic_programming/homework_solutions/test/lesson21/OOP/electronics/TabletTest.java
@@ -1,7 +1,5 @@
 package lesson21.OOP.electronics;
 
-package lesson21.OOP.electronics;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;

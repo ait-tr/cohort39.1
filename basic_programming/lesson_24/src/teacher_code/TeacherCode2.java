b1593fc593b8c5e2d65969389c2464f40f2cc31e
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-12 20:56:13 +0100

    lesson 24

--- basic_programming/lesson_24/src/teacher_code/TeacherCode2.java
+++ basic_programming/lesson_24/src/teacher_code/TeacherCode2.java
@@ -1,4 +1,15 @@
 package teacher_code;
 
 public class TeacherCode2 {
+    static int counter = 0;
+
+    public static void main(String[] args) {
+        directRecursive();
+    }
+
+    static void directRecursive() {
+        System.out.print(counter + " ");
+        counter++;
+        directRecursive();
+    }
 }

fa01ea34f1506091041837769037ef88df89defb
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-12 20:53:22 +0100

    lesson 24

--- basic_programming/lesson_24/src/teacher_code/TeacherCode.java
+++ basic_programming/lesson_24/src/teacher_code/TeacherCode.java
@@ -10,11 +10,43 @@ public class TeacherCode {
         // n! = n * (n - 1) * (n - 2) *... * 1;
         // {0, 1, 2, 6, 24, 120}
 
-        System.out.println(cacheFactorialIterative(5));
-        System.out.println(cacheFactorialIterative(3));
-        System.out.println(cacheFactorialIterative(4));
+        int number = 5;
 
+        System.out.println("factorialRecursive(5) = " + factorialRecursive(number));
+
+        int result = 1;
+
+        while (number > 0 && number != 0 && number != 1) {
+            result *= number;
+            number--;
+        }
+
+        System.out.println("result = " + result);
     }
+
+    // time capacity = O(n)
+    // space capacity (heap) = O (1)
+    // space capacity (stack) = O(n) (тк каждый раз рекурсивно вызываем метод
+    // factorialRecursive, что занимает место в стеке и может привести к ошибке переполнения стека - StackOverFlowError)
+    public static int factorialRecursive(int number) { // 1
+        if(number < 0) {
+            return -1;
+        } else if (number == 0) {
+            return 0;
+        } else if (number == 1) {
+            return 1;
+        }
+
+        // 5! = 5 * 4!
+        // 4! = 4 * 3!
+        // 3! = 3 * 2!
+        // 2! = 2 * 1!
+        // 1! = 1
+
+        return number * factorialRecursive(number - 1); //  5 * (4 * (3 * (2 * (1 * (1))))
+    }
+
+
 
     // time capacity = from O(1) to O(n)
     // space capacity (heap) = O(n) тк мы увеличиваем размер массива factorialValues
@@ -68,6 +100,9 @@ public class TeacherCode {
 
         return result;
     }
+
+
+
 
 }
 

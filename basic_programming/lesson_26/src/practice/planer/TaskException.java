4878211c89e7f3940cf5f972f57287a89c7817e3
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-17 20:37:38 +0100

    lesson 27

---
+++ basic_programming/lesson_26/src/practice/planer/TaskException.java
@@ -0,0 +1,7 @@
+package practice.planer;
+
+public class TaskException extends Exception {
+    public TaskException(String message) {
+        super(message);
+    }
+}
--- basic_programming/lesson_26/src/practice/planer/Tasks.java
+++ basic_programming/lesson_26/src/practice/planer/Tasks.java
@@ -1,5 +1,7 @@
 package practice.planer;
 
+import java.util.Arrays;
+
 public class Tasks<T> {
     private T[] array;
 
@@ -14,14 +16,27 @@ public class Tasks<T> {
     // метод добавления нового элемента с расширением массива
     public void addTask(T t) {
         // ...
+        // [1, 2, 3] + 4 -> [1, 2, 3, 0] + 4 -> [1, 2, 3, 0]
+        array = Arrays.copyOf(array, array.length + 1);
+        array[array.length - 1] = t;
     }
 
     // метод удаления элемента из массива с сокращением размера массива
-    public void removeTask(T t) {
+    public void removeTask(T t) throws TaskException {
+        if (!contain(t)) {
+            throw new TaskException("данной задачи нет в списке дел!");
+        }
+
+
 
     }
 
     public boolean contain(T t) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(t)) {
+                return true;
+            }
+        }
 
         return false;
     }

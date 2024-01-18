8a6ac9f5319d58446143b6d2fb479b0bcc1a472b
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-18 19:16:56 +0100

    lesson 27

--- basic_programming/consultation/src/consultation2024_01_18/teachers_sandbox/TeacherSandbox.java
+++ basic_programming/consultation/src/consultation2024_01_18/teachers_sandbox/TeacherSandbox.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class TeacherSandbox {
     public static void main(String[] args) {
         System.out.println("hello, students!");
+        System.out.println("hello, again!");
     }
 
 }

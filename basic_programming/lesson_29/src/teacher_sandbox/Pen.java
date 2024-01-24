6259109378137c12f58cd83eb9fb5e9cf171f394
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-24 21:19:39 +0100

    lesson 29 hash code and equals. Set

--- basic_programming/lesson_29/src/teacher_sandbox/Pen.java
+++ basic_programming/lesson_29/src/teacher_sandbox/Pen.java
@@ -17,7 +17,7 @@ public class Pen {
 
     private void draw(){
         if (canDraw) {
-            System.out.println("drawing");
+            System.out.println(this.toString() + " drawing");
         } else {
             throw new RuntimeException("Pen cant draw!");
         }
@@ -82,6 +82,32 @@ public class Pen {
     }
 
 
+    public static void main(String[] args) {
+        Pen pen1 = new Pen(TypeOfDrawing.BALLPOINT, "blue", true);
+        Pen pen2 = new Pen(TypeOfDrawing.BALLPOINT, "blue", true);
+
+        System.out.println("pen1 == pen2 = " + (pen1 == pen2));
+        System.out.println("pen1.equals(pen2) = " + pen1.equals(pen2));
+        System.out.println("pen1.hashCode() = " + pen1.hashCode());
+        System.out.println("pen2.hashCode() = " + pen2.hashCode());
+
+        pen1.setInkColor("black");
+        System.out.println("pen1.hashCode() = " + pen1.hashCode());
+        System.out.println("pen2.hashCode() = " + pen2.hashCode());
+
+        pen1.setInkColor("blue");
+        System.out.println("pen1.hashCode() = " + pen1.hashCode());
+        System.out.println("pen2.hashCode() = " + pen2.hashCode());
+
+        pen2.setCanDraw(false);
+        pen1.setInkColor("blue");
+        System.out.println("pen1.hashCode() = " + pen1.hashCode());
+        System.out.println("pen2.hashCode() = " + pen2.hashCode());
+
+        pen1.draw();
+        pen2.draw();
+
+    }
 
     private enum TypeOfDrawing {
         BALLPOINT, INK;

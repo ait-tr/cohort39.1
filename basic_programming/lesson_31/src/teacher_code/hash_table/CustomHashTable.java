1a6171769f31c911437840dfe5e96d00c2a8cd31
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-29 21:04:06 +0100

    lesson 31 hash table

--- basic_programming/lesson_31/src/teacher_code/hash_table/CustomHashTable.java
+++ basic_programming/lesson_31/src/teacher_code/hash_table/CustomHashTable.java
@@ -1,4 +1,19 @@
 package teacher_code.hash_table;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ *  класс CustomHashTable искользуется для практики и изучения структуры данных хеш-таблиц. <p>
+ *  Данный класс должен изучать и показывать, как влияют методы hashCode и equals на функционирование хеш-таблиц,
+ *  лежащих в основе таких популярных структур данных, как HashSet и HashMap.
+ */
 public class CustomHashTable {
+
+    private ArrayList <LinkedList<Movie>> table = new ArrayList<>();
+
+    public CustomHashTable() {
+    }
+
+
 }

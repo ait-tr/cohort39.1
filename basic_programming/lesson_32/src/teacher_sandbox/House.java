c186eec7e6dcc97831f40a307c0a038fdda6a013
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-31 21:28:25 +0100

    lesson 32 tree

---
+++ basic_programming/lesson_32/src/teacher_sandbox/House.java
@@ -0,0 +1,11 @@
+package teacher_sandbox;
+
+public class House {
+    private double area;
+    private String address;
+
+    public House(double area, String address) {
+        this.area = area;
+        this.address = address;
+    }
+}

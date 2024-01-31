65d8622da1de85e74a945629ec95a6acee1a0115
imiftakhov <ilias.miftakhov@yandex.ru>
2024-01-31 21:30:48 +0100

    lesson 32 tree

--- basic_programming/lesson_32/src/teacher_sandbox/House.java
+++ basic_programming/lesson_32/src/teacher_sandbox/House.java
@@ -1,5 +1,7 @@
 package teacher_sandbox;
 
+import java.util.ArrayList;
+
 public class House {
     private double area;
     private String address;
@@ -7,5 +9,31 @@ public class House {
     public House(double area, String address) {
         this.area = area;
         this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "House{" +
+                "area=" + area +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        House house1 = new House(100, "aaa");
+        House house2 = new House(150, "bbb");
+        House house3 = new House(80, "abc");
+        House house4 = new House(36, "ccc");
+
+        ArrayList<House> houses = new ArrayList<>();
+
+        houses.add(house1);
+        houses.add(house4);
+        houses.add(house2);
+        houses.add(house3);
+
+        for (House house : houses) {
+            System.out.println(house);
+        }
     }
 }

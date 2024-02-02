91f3edd23b93e3eb3b3d1833cbb3b5c39c7d873d
imiftakhov <ilias.miftakhov@yandex.ru>
2024-02-02 20:59:58 +0100

    lesson 33 input output in java

--- basic_programming/lesson_33/src/teacher_code/TeacherCode2.java
+++ basic_programming/lesson_33/src/teacher_code/TeacherCode2.java
@@ -2,17 +2,19 @@ package teacher_code;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class TeacherCode2 {
+    private static String pathToFile = "basic_programming/lesson_33/src/teacher_code/file2.txt";
 
     public static void main(String[] args) {
-        tryFileInputStream();
+        tryFileOutputStream();
     }
 
     public static void tryWithResources() {
-        File file = new File("basic_programming/lesson_33/src/teacher_code/file.txt");
+        File file = new File(pathToFile);
 
         // подобная конструкция называется try with resources,
         // в данной конструкции в круглых скобках блока try указывается операция,
@@ -32,7 +34,7 @@ public class TeacherCode2 {
     }
 
     public static void readFileWithScanner() {
-        File file = new File("basic_programming/lesson_33/src/teacher_code/file.txt");
+        File file = new File(pathToFile);
 
         try {
             Scanner scanner = new Scanner(file);
@@ -46,11 +48,20 @@ public class TeacherCode2 {
     }
 
     public static void tryFileInputStream() {
-        try (FileInputStream fis = new FileInputStream("basic_programming/lesson_33/src/teacher_code/file.txt")) {
+        try (FileInputStream fis = new FileInputStream(pathToFile)) {
             int i;
             while ((i = fis.read()) != -1) {
                 System.out.println((char) i);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void tryFileOutputStream() {
+        try (FileOutputStream fos = new FileOutputStream(pathToFile)) {
+            String data = "Hello, Java!";
+            fos.write(data.getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }

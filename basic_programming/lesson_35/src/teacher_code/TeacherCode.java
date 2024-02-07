db88c6607c3d8f472a106e363364cad3aaea2bd7
imiftakhov <ilias.miftakhov@yandex.ru>
2024-02-07 20:28:51 +0100

    lesson 35 date time api

--- basic_programming/lesson_35/src/teacher_code/TeacherCode.java
+++ basic_programming/lesson_35/src/teacher_code/TeacherCode.java
@@ -1,11 +1,27 @@
 package teacher_code;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Set;
 
 public class TeacherCode {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        LocalDate date = LocalDate.of(2017, 2, 17);
 
+        LocalDate someDay = date.plusWeeks(10);
+        System.out.println(someDay);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println(now);
+        System.out.println(now);
+
+        // программа будет "спать" 2000 мс
+        Thread.sleep(2000);
+
+        System.out.println(now);
+        System.out.println(now);
     }
 }

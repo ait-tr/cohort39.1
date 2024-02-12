0e5c26b4ad3e1decc90af58ee4adfd666cf7785b
imiftakhov <ilias.miftakhov@yandex.ru>
2024-02-12 20:48:12 +0100

    lesson 37 functional interfaces

--- basic_programming/lesson_37/src/teacher_code/MyClass.java
+++ basic_programming/lesson_37/src/teacher_code/MyClass.java
@@ -5,4 +5,9 @@ public class MyClass implements MyInterface{
     public int calculate(int a, int b) {
         return a * b;
     }
+
+    @Override
+    public int calculate(int a, int b, int c) {
+        return Math.abs(a * b * c);
+    }
 }
--- basic_programming/lesson_37/src/teacher_code/TeacherCode2.java
+++ basic_programming/lesson_37/src/teacher_code/TeacherCode2.java
@@ -4,5 +4,23 @@ public class TeacherCode2 {
     public static void main(String[] args) {
         MyClass myClass = new MyClass();
 
+        System.out.println("Using of abstract method calculate(10, 20) = " + myClass.calculate(10, 20));
+        System.out.println("Using of default method calculate(10, -20, 30) = " + myClass.calculate(10, -20, 30));
+        System.out.println("Using of static method MyInterface.calculate(10) = " + MyInterface.calculate(10));
+
+        MyInterface substractor  = (a, b) -> a - b;
+
+        System.out.println("substractor.calculate(10, 15) = " + substractor.calculate(10, 15));
+
+
+        // данный пример показывает то, что логика может быть более сложной
+        MyInterface anotherOne = (a, b) -> {
+            if (a < 10) {
+                return b * a;
+            } else {
+                return b - a;
+            }
+        };
+
     }
 }

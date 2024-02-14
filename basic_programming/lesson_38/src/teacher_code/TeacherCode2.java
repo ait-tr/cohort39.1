d5a52b69e891c7c02f35d6ce54343f97b0b2871d
imiftakhov <ilias.miftakhov@yandex.ru>
2024-02-14 21:00:43 +0100

    lesson 38 stream api

--- basic_programming/lesson_38/src/teacher_code/TeacherCode2.java
+++ basic_programming/lesson_38/src/teacher_code/TeacherCode2.java
@@ -13,27 +13,32 @@ import java.util.stream.Stream;
 
 public class TeacherCode2 {
     public static void main(String[] args) throws IOException {
-        Collection<Integer> collection = Set.of(1, 2, 3, 4, 5, 6, 7);
-        Integer oldOdd = 0;
-        for (Integer i : collection) {
-            if (i % 2 != 0) {
-                oldOdd += i;
-            }
-        }
+//        Collection<Integer> collection = Set.of(1, 2, 3, 4, 5, 6, 7);
+//        Integer oldOdd = 0;
+//        for (Integer i : collection) {
+//            if (i % 2 != 0) {
+//                oldOdd += i;
+//            }
+//        }
+//
+//        Stream<Integer> stream = getStreamFromFile();
+//
+//        //IntStream intStream = getStreamFromPrimitiveValues();
+//
+//        Integer oddsSum = stream
+//                .filter(e -> e % 2 == 1)
+//                .reduce((c1, c2) -> c1 + c2)
+//                .orElse(0);
+//
+//
+//        System.out.println("oldOdd = " + oldOdd);
+//        System.out.println("oddsSum = " + oddsSum);
+//        System.out.println("oldOdd == oddsSum = " + (oldOdd == oddsSum));
 
-        Stream<Integer> stream = getStreamFromFile();
 
-        //IntStream intStream = getStreamFromPrimitiveValues();
-
-        Integer oddsSum = stream
-                .filter(e -> e % 2 == 1)
-                .reduce((c1, c2) -> c1 + c2)
-                .orElse(0);
-
-
-        System.out.println("oldOdd = " + oldOdd);
-        System.out.println("oddsSum = " + oddsSum);
-        System.out.println("oldOdd == oddsSum = " + (oldOdd == oddsSum));
+        getStreamFromArray()
+                .filter(e -> e > 3)
+                .forEach(e -> System.out.println(e));
 
 
     }

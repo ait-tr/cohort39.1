36a83b79552d1e3120bef91153f7af30de60b146
imiftakhov <ilias.miftakhov@yandex.ru>
2024-02-29 17:52:34 +0100

    consultation

---
+++ basic_programming/consultation/src/consultation2024_02_29/teachers_sandbox/TeacherSandbox.java
@@ -0,0 +1,7 @@
+package consultation2024_02_29.teachers_sandbox;
+
+import java.math.BigInteger;
+
+public class TeacherSandbox {
+
+}
--- basic_programming/lesson_44/src/practice/Algorithmic.java
+++ basic_programming/lesson_44/src/practice/Algorithmic.java
@@ -6,6 +6,13 @@ public class Algorithmic {
     // 1 -> 1
     // 120 -> 21
     public static int reverse(int num) {
+        StringBuilder builder = new StringBuilder();
+        boolean isNegative = num < 0;
+        builder.append(Math.abs(num));
+        builder.reverse();
+
+
+        ///
         return 0;
     }
 }

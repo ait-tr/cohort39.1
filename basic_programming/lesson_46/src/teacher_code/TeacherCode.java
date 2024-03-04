8d37e1b994a2f88b9a53f8b2a01fcf3df667ca4c
imiftakhov <ilias.miftakhov@yandex.ru>
2024-03-04 21:32:42 +0100

    lesson 46

--- basic_programming/lesson_46/src/teacher_code/TeacherCode.java
+++ basic_programming/lesson_46/src/teacher_code/TeacherCode.java
@@ -1,27 +1,99 @@
 package teacher_code;
 
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TeacherCode {
-    static int counter = 0;
+    private static final Object LOCK = new Object();
+    static volatile int counter = 0;
+
+
+    // A -> copy counter value in to  processor cache -> do some logic in cache  -> return new val from proc cache into ram
+    // B -> copy counter value in to  processor cache -> do some logic in cache  -> return new val from proc cache into ram
+
+    // after volatile:
+    // everything in ram
+    // A -> processor do it's job in ram without cache
+    // B -> processor do it's job in ram without cache
+
+
+
+    static AtomicInteger atomicCounter = new AtomicInteger(0);
 
     public static void main(String[] args) throws InterruptedException {
-        increase();
-        increase();
+//        increase(); //  A
+//        increase(); //  B
+//
+//        Thread.sleep(1000);
+//
+//        System.out.println("counter = " + counter);
+//        System.out.println("atomicCounter = " + atomicCounter.get());
 
-        Thread.sleep(2000);
+        Object lock_a = new Object();
+        Object lock_b = new Object();
 
-        System.out.println("counter = " + counter);
+        Thread a = new Thread(() -> {
+            try {
+                deadlockExample(lock_a, lock_b);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        Thread b = new Thread(() -> {
+            try {
+                deadlockExample(lock_b, lock_a);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        a.start();
+        b.start();
     }
 
-    private static void increase() {
+
+    public static void deadlockExample(Object first, Object second) throws InterruptedException {
+        synchronized (first) {
+            Thread.sleep(300);
+            synchronized (second) {
+                System.out.println("synchronized");
+            }
+        }
+    }
+
+    private static void increase() throws InterruptedException {
         Thread thread = new Thread(() ->
         {
             for (int i = 0; i < 1_000_000; i++) {
-                counter++;
+                // данный метод увеличивает значение данной переменной на 1
+                atomicCounter.incrementAndGet();
+//                increaseCounter();
+
+                synchronized (LOCK) {
+                    // копируем текущее значение
+                    // увеличиваем текущее значение на 1
+                    // перезаписываем значение в переменную counter
+                    counter++; // 10  - 12
+
+                    try {
+                        LOCK.wait();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+
             }
         });
         thread.start();
     }
+
+    private synchronized static void increaseCounter() {
+        // difficult logic
+        counter++;
+    }
+
+
+
 }
 
 

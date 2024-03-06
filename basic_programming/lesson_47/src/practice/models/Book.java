727ee4a3b89b7a31bf050e3eb52169a265e4ee85
imiftakhov <ilias.miftakhov@yandex.ru>
2024-03-06 19:22:48 +0100

    lesson 46

--- basic_programming/lesson_47/src/practice/models/Book.java
+++ basic_programming/lesson_47/src/practice/models/Book.java
@@ -2,4 +2,64 @@ package practice.models;
 
 // Модель
 public class Book {
+    private Author author;
+    private String title;
+    private Genre genre;
+
+    public Book(Author author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public Book(Author author, String title, Genre genre) {
+        this.author = author;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+
+        if (getAuthor() != null ? !getAuthor().equals(book.getAuthor()) : book.getAuthor() != null) return false;
+        if (getTitle() != null ? !getTitle().equals(book.getTitle()) : book.getTitle() != null) return false;
+        return getGenre() == book.getGenre();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getAuthor() != null ? getAuthor().hashCode() : 0;
+        result = 31 * result + (getTitle() != null ? getTitle().hashCode() : 0);
+        result = 31 * result + (getGenre() != null ? getGenre().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author=" + author +
+                ", title='" + title + '\'' +
+                ", genre=" + genre +
+                '}';
+    }
+
+    public enum Genre{
+        CLASSIC,
+        DETECTIVE,
+        LOVE_STORY,
+        SCIENCE;
+    }
 }
--- basic_programming/lesson_47/src/practice/models/Reader.java
+++ basic_programming/lesson_47/src/practice/models/Reader.java
@@ -2,4 +2,48 @@ package practice.models;
 
 // Модель
 public class Reader {
+    private String mail;
+    private String password;
+
+    public Reader(String mail, String password) {
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reader reader)) return false;
+
+        return getMail() != null ? getMail().equals(reader.getMail()) : reader.getMail() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getMail() != null ? getMail().hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Reader{" +
+                "mail='" + mail + '\'' +
+                ", password='" + "******" + '\'' +
+                '}';
+    }
 }
